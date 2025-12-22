8539b2aaeada08ea0371734bbe5a77f6ed268c0b
muliroZ <murilomega100@gmail.com>
2025-12-22 17:03:46 -0300

    projeto iniciado

---
+++ src/test/java/com/muriloscorp/codesv/CodeSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muriloscorp.codesv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
